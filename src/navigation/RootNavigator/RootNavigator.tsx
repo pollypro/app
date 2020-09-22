6b60852796c2854374b4c0cc505c567b01ef4285
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-23 01:56:37 +0300

    connect to redux, map token to props, render AppNavigation if there's a token

--- src/navigation/RootNavigator/RootNavigator.tsx
+++ src/navigation/RootNavigator/RootNavigator.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from '../AuthNavigator/AuthNavigation';
+import AppNavigation from '../AppNavigator/AppNavigation';
 import NavigationService from '../NavigationService';
+import { State } from '../../types/redux';
 
-const RootNavigator: FC = () => (
+const reduxConnector = connect(({ auth }: State) => ({ token: auth.token }));
+
+type Props = ConnectedProps<typeof reduxConnector>;
+
+const RootNavigator: FC<Props> = ({ token }) => (
   <NavigationContainer ref={(navigatorRef) => NavigationService.setTopLevelNavigator(navigatorRef)}>
-    <AuthNavigator />
+    {token ? <AppNavigation /> : <AuthNavigator />}
   </NavigationContainer>
 );
 
-export default RootNavigator;
+export default reduxConnector(RootNavigator);
