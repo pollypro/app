4f2de1eefc41b6352c66be6777b480f278f81090
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-22 03:50:10 +0300

    render AuthStack.Screen for ForgetPassword

--- src/navigation/AuthNavigator/AuthNavigation.tsx
+++ src/navigation/AuthNavigator/AuthNavigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Routes } from '../routes';
-import { SignIn } from '../../screens';
+import { ForgetPassword, SignIn } from '../../screens';
 import { AuthParamList } from './types';
 
 const AuthStack = createStackNavigator<AuthParamList>();
@@ -9,6 +9,11 @@ const AuthStack = createStackNavigator<AuthParamList>();
 const AuthStackNavigator = () => (
   <AuthStack.Navigator initialRouteName={Routes.SignIn} mode="card">
     <AuthStack.Screen name={Routes.SignIn} component={SignIn} options={{ headerShown: false }} />
+    <AuthStack.Screen
+      name={Routes.ForgetPassword}
+      component={ForgetPassword}
+      options={{ title: 'Forget Password' }}
+    />
   </AuthStack.Navigator>
 );
 
