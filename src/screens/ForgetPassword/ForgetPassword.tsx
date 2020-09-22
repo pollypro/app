565ce9c9d6465b3d826d1d717b592c245cb8675a
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-22 03:49:29 +0300

    add empty ForgetPassword screen component

---
+++ src/screens/ForgetPassword/ForgetPassword.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+const ForgetPassword: FC = () => {
+  return (
+    <View>
+      <Text>Forget password screen</Text>
+    </View>
+  );
+};
+
+export default ForgetPassword;
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,2 @@
+export { default as ForgetPassword } from './ForgetPassword/ForgetPassword';
 export { default as SignIn } from './SignIn/SignIn';
