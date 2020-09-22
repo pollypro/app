457284f19769e134bab8f11f6f3d3c74f5b6dd2e
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-22 17:12:56 +0300

    add redux types, add reusable redux type, add State type with auth node

---
+++ src/types/redux.ts
@@ -0,0 +1,43 @@
+// imports from utils
+import { IHttpError } from '../utils/httpError';
+
+// imports from types
+
+type ListNode<T> = {
+  isLoading: boolean;
+  items: T[];
+  error?: IHttpError;
+};
+
+type ItemNode<T> = {
+  isLoading: boolean;
+  item?: T;
+  error?: IHttpError;
+};
+
+type UpdatingNode = {
+  isLoading: boolean;
+  id?: string;
+  error?: IHttpError;
+};
+
+type CreatingNode<T> = {
+  isLoading: boolean;
+  item?: T;
+  error?: IHttpError;
+};
+
+type DeletingNode = {
+  isLoading: boolean;
+  id?: string;
+  error?: IHttpError;
+};
+
+export type State = {
+  auth: {
+    isLoading: boolean;
+    isAuthenticated: boolean;
+    token?: string;
+    error?: IHttpError;
+  };
+};
