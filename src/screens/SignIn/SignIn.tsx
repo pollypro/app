73b20e7f60cb8f7813f01a7c874119b926c34ff9
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-22 03:33:20 +0300

    add initial SignIn screen component

---
+++ src/screens/SignIn/SignIn.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+const SignIn: FC = () => {
+  return (
+    <View>
+      <Text>Sign in screen</Text>
+    </View>
+  );
+};
+
+export default SignIn;
