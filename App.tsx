3f18f29b87d373d013cb1761ecdb6ea33ae2df28
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-22 17:17:50 +0300

    render ReduxProvider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 // imports from vendors
 import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+// imports from navigation
 import AppNavigator from './src/navigation/AppNavigator/AppNavigator';
 
+import reduxStore from './src/reduxStore';
+
 const App = () => (
-  <SafeAreaProvider>
-    <AppNavigator />
-  </SafeAreaProvider>
+  <ReduxProvider store={reduxStore}>
+    <SafeAreaProvider>
+      <AppNavigator />
+    </SafeAreaProvider>
+  </ReduxProvider>
 );
 
 export default App;
