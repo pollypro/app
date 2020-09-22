bdb7fd10b06f5a1b2260e92b80af4901baccf558
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-22 04:43:07 +0300

    fix imports

--- src/navigation/AppNavigator/AppNavigator.tsx
+++ src/navigation/AppNavigator/AppNavigator.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AuthNavigator from '../authNavigator';
-import NavigationService from '../navigationService';
+import AuthNavigator from '../AuthNavigator/AuthNavigation';
+import NavigationService from '../NavigationService';
 
 const AppNavigator: FC = () => (
   <NavigationContainer ref={(navigatorRef) => NavigationService.setTopLevelNavigator(navigatorRef)}>
     <AuthNavigator />
   </NavigationContainer>
 );
+
+export default AppNavigator;
