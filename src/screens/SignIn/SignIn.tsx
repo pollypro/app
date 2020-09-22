b93bff6520692fb4abc5af5652f0831326c5a73e
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-23 02:13:35 +0300

    wrap everything with ScreenLayout shared component

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,11 +1,14 @@
 // imports from vendors
 import React, { FC, useState } from 'react';
 import { ConnectedProps, connect } from 'react-redux';
-import { View, ImageBackground, SafeAreaView, Image, Text } from 'react-native';
+import { Image, Text } from 'react-native';
 import { InputItem, List, Button } from '@ant-design/react-native';
 
 // imports from modules
 import { authenticate } from '../../modules/auth';
+
+// imports from shared
+import { ScreenLayout } from '../shared';
 
 // imports from styles
 import styles from './styles';
@@ -23,40 +26,20 @@ const SignIn: FC<Props> = ({ authenticate }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require('../../resources/images/background.png')}
-        style={styles.background}
-        resizeMode="cover"
+    <ScreenLayout centered>
+      <Image source={require('./polly-logo.png')} style={styles.logo} />
+      <List style={styles.list}>
+        <InputItem type="email-address" placeholder="E-mail" value={email} onChange={setEmail} />
+        <InputItem type="password" placeholder="Пароль" value={password} onChange={setPassword} />
+      </List>
+      <Button
+        style={styles.submitButton}
+        activeStyle={styles.submitButtonActive}
+        onPress={handleSubmit}
       >
-        <SafeAreaView>
-          <View style={styles.content}>
-            <Image source={require('./polly-logo.png')} style={styles.logo} />
-            <List style={styles.list}>
-              <InputItem
-                type="email-address"
-                placeholder="E-mail"
-                value={email}
-                onChange={setEmail}
-              />
-              <InputItem
-                type="password"
-                placeholder="Пароль"
-                value={password}
-                onChange={setPassword}
-              />
-            </List>
-            <Button
-              style={styles.submitButton}
-              activeStyle={styles.submitButtonActive}
-              onPress={handleSubmit}
-            >
-              <Text style={styles.submitButtonText}>Войти</Text>
-            </Button>
-          </View>
-        </SafeAreaView>
-      </ImageBackground>
-    </View>
+        <Text style={styles.submitButtonText}>Войти</Text>
+      </Button>
+    </ScreenLayout>
   );
 };
 
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,26 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  background: {
-    width: '100%',
-    height: '100%',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  content: {
-    padding: 20,
-  },
   logo: {
     alignSelf: 'center',
     marginBottom: 38,
   },
-  list: { width: 300, marginBottom: 29 },
-  submitButton: { borderRadius: 20, backgroundColor: '#2662F0', width: 126, height: 36 },
-  submitButtonActive: { backgroundColor: '#0f4cdd' },
-  submitButtonText: { color: 'white', fontSize: 14 },
+  list: {
+    width: 300,
+    marginBottom: 29,
+  },
+  submitButton: {
+    borderRadius: 20,
+    backgroundColor: '#2662F0',
+    width: 126,
+    height: 36,
+  },
+  submitButtonActive: {
+    backgroundColor: '#0f4cdd',
+  },
+  submitButtonText: {
+    color: 'white',
+    fontSize: 14,
+  },
 });
