5123dd25a57b13fc9d3c5d88523b83ef4549a926
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-23 01:09:49 +0300

    add empty Services screen

---
+++ src/screens/Services/Services.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+const Services: FC = () => {
+  return (
+    <View>
+      <Text>Services screen</Text>
+    </View>
+  );
+};
+
+export default Services;
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,3 @@
 export { default as ForgetPassword } from './ForgetPassword/ForgetPassword';
+export { default as Services } from './Services/Services';
 export { default as SignIn } from './SignIn/SignIn';
