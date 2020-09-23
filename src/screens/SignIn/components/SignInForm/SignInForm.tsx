29704d71810c2765ae5cc24145aa99cf617ce9c4
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-23 04:28:10 +0300

    use @ui-kitten components

--- src/screens/SignIn/components/SignInForm/SignInForm.tsx
+++ src/screens/SignIn/components/SignInForm/SignInForm.tsx
@@ -1,41 +1,58 @@
 // imports from vendors
 import React, { FC, useState } from 'react';
 import { ConnectedProps, connect } from 'react-redux';
-import { Text } from 'react-native';
-import { InputItem, List, Button } from '@ant-design/react-native';
+import { View, TouchableWithoutFeedback } from 'react-native';
+import { Button, Icon, Input } from '@ui-kitten/components';
 
 // imports from modules
 import { authenticate } from '../../../../modules/auth';
 
+// imports from types
+import { State } from '../../../../types/redux';
+
 // imports from styles
 import styles from './styles';
 
-const reduxConnector = connect(null, { authenticate });
+const reduxConnector = connect(({ auth }: State) => ({ auth }), { authenticate });
 
 type Props = ConnectedProps<typeof reduxConnector>;
 
-const SignInForm: FC<Props> = ({ authenticate }) => {
+const SignInForm: FC<Props> = ({ auth, authenticate }) => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [secureTextEntry, setSecureTextEntry] = React.useState(true);
+
+  const toggleSecureEntry = () => {
+    setSecureTextEntry(!secureTextEntry);
+  };
 
   const handleSubmit = () => {
     authenticate(email, password);
   };
 
   return (
-    <>
-      <List style={styles.list}>
-        <InputItem type="email-address" placeholder="E-mail" value={email} onChange={setEmail} />
-        <InputItem type="password" placeholder="Пароль" value={password} onChange={setPassword} />
-      </List>
-      <Button
-        style={styles.submitButton}
-        activeStyle={styles.submitButtonActive}
-        onPress={handleSubmit}
-      >
-        <Text style={styles.submitButtonText}>Войти</Text>
+    <View style={styles.form}>
+      <Input
+        textContentType="emailAddress"
+        placeholder="E-mail"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Input
+        value={password}
+        placeholder="Пароль"
+        onChangeText={setPassword}
+        secureTextEntry={secureTextEntry}
+        accessoryRight={(props) => (
+          <TouchableWithoutFeedback onPress={toggleSecureEntry}>
+            <Icon {...props} name={secureTextEntry ? 'eye-off' : 'eye'} />
+          </TouchableWithoutFeedback>
+        )}
+      />
+      <Button onPress={handleSubmit} style={styles.submitButton}>
+        Войти
       </Button>
-    </>
+    </View>
   );
 };
 
--- src/screens/SignIn/components/SignInForm/styles.ts
+++ src/screens/SignIn/components/SignInForm/styles.ts
@@ -1,21 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  list: {
+  form: {
     width: 300,
-    marginBottom: 29,
   },
   submitButton: {
-    borderRadius: 20,
-    backgroundColor: '#2662F0',
-    width: 126,
-    height: 36,
-  },
-  submitButtonActive: {
-    backgroundColor: '#0f4cdd',
-  },
-  submitButtonText: {
-    color: 'white',
-    fontSize: 14,
+    marginTop: 30,
   },
 });
