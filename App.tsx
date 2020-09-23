5fe0051801c58319dd00b2f972b9b1f1922a9622
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-23 04:27:35 +0300

    render IconRegistry

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 // imports from vendors
 import React from 'react';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 
 import { Provider as ReduxProvider } from 'react-redux';
@@ -12,13 +13,16 @@ import RootNavigator from './src/navigation/RootNavigator/RootNavigator';
 import reduxStore from './src/reduxStore';
 
 const App = () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <ReduxProvider store={reduxStore}>
-      <SafeAreaProvider>
-        <RootNavigator />
-      </SafeAreaProvider>
-    </ReduxProvider>
-  </ApplicationProvider>
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <ReduxProvider store={reduxStore}>
+        <SafeAreaProvider>
+          <RootNavigator />
+        </SafeAreaProvider>
+      </ReduxProvider>
+    </ApplicationProvider>
+  </>
 );
 
 export default App;
