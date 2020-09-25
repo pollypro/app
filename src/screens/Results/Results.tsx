50f046f982217f04f3d7169748380cb4f62812e1
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-25 16:19:48 +0300

    add empty Results screen component

---
+++ src/screens/Results/Results.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+const Results: FC = () => {
+  return (
+    <View>
+      <Text>Results screen</Text>
+    </View>
+  );
+};
+
+export default Results;
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,5 @@
 export { default as ForgetPassword } from './ForgetPassword/ForgetPassword';
+export { default as Results } from './Results/Results';
 export { default as Service } from './Service/Service';
 export { default as Services } from './Services/Services';
 export { default as SignIn } from './SignIn/SignIn';
