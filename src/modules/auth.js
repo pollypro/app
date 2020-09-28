1e8920733c6d331d1c1c15f8bec566cb6e1f8445
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-29 02:32:38 +0300

    add AUTH_REPLACE action, replaceAuth action creator. store the token in AsyncStorage

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,8 +1,15 @@
 // imports from vendors
 import { createReducer } from 'redux-act';
+import AsyncStorage from '@react-native-community/async-storage';
 
 // imports from utils
-import { requestAction, successAction, failureAction, cleanupAction } from '../utils/redux';
+import {
+  requestAction,
+  successAction,
+  failureAction,
+  cleanupAction,
+  replaceAction,
+} from '../utils/redux';
 import { coreApiUrl } from '../utils/urls';
 import httpClient from '../utils/httpClient';
 
@@ -11,6 +18,7 @@ export const AUTH_REQUEST = requestAction(MODULE_NAME_ROOT);
 export const AUTH_SUCCESS = successAction(MODULE_NAME_ROOT);
 export const AUTH_FAILURE = failureAction(MODULE_NAME_ROOT);
 export const AUTH_CLEANUP = cleanupAction(MODULE_NAME_ROOT);
+export const AUTH_REPLACE = replaceAction(MODULE_NAME_ROOT);
 
 const initialState = {
   isLoading: false,
@@ -30,6 +38,11 @@ export default createReducer(
       error: null,
     }),
     [AUTH_FAILURE]: (state, { error }) => ({ ...state, isLoading: false, error }),
+    [AUTH_REPLACE]: (state, { token }) => ({
+      ...initialState,
+      isAuthenticated: true,
+      token,
+    }),
     [AUTH_CLEANUP]: () => initialState,
   },
   initialState,
@@ -42,9 +55,10 @@ export const authenticate = (email, password) => (dispatch) => {
 
   return httpClient
     .post(coreApiUrl('/auth/authenticate'), { email, password })
-    .then(({ data }) => {
+    .then(async ({ data }) => {
       dispatch({ type: AUTH_SUCCESS, payload: { token: data.token } });
       httpClient.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+      await AsyncStorage.setItem('token', data.token);
       return data;
     })
     .catch(({ error }) => {
@@ -52,7 +66,14 @@ export const authenticate = (email, password) => (dispatch) => {
     });
 };
 
-export const destroyAuth = () => (dispatch) => {
+export const replaceAuth = (token) => async (dispatch) => {
+  dispatch({ type: AUTH_REPLACE, payload: { token } });
+  httpClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  await AsyncStorage.setItem('token', token);
+};
+
+export const destroyAuth = () => async (dispatch) => {
   delete httpClient.defaults.headers.common['Authorization'];
+  await AsyncStorage.removeItem('token');
   dispatch(cleanupAuth());
 };
