f30cf34c32ab6a56524fe083eea3524b9b24371c
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-29 01:26:24 +0300

    pass AppDrawerContent as drawerContent, do not render header

--- src/navigation/AppNavigator/AppNavigation.tsx
+++ src/navigation/AppNavigator/AppNavigation.tsx
@@ -4,19 +4,23 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Routes } from '../routes';
 import { Service, Services } from '../../screens';
 import { AppParamList } from './types';
+import AppDrawerContent from './AppDrawerContent';
 
 const AppDrawer = createDrawerNavigator<AppParamList>();
 const ServicesStack = createStackNavigator<{ Services: undefined; Service: undefined }>();
 
 const ServicesStackNavigator = () => (
   <ServicesStack.Navigator>
-    <ServicesStack.Screen name="Services" component={Services} />
-    <ServicesStack.Screen name="Service" component={Service} />
+    <ServicesStack.Screen name="Services" component={Services} options={{ headerShown: false }} />
+    <ServicesStack.Screen name="Service" component={Service} options={{ headerShown: false }} />
   </ServicesStack.Navigator>
 );
 
 const AppDrawerNavigator = () => (
-  <AppDrawer.Navigator initialRouteName={Routes.Services}>
+  <AppDrawer.Navigator
+    initialRouteName={Routes.Services}
+    drawerContent={(props) => <AppDrawerContent {...props} />}
+  >
     <AppDrawer.Screen name={Routes.Services} component={ServicesStackNavigator} />
   </AppDrawer.Navigator>
 );
