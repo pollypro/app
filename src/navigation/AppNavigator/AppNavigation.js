117e19588d5c5b425eb2988d6cb7a3c6308da8f1
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-29 01:32:29 +0300

    AppNavigator: tsx -> js, add ResultsStackStackNavigator

--- src/navigation/AppNavigator/AppNavigation.js
+++ src/navigation/AppNavigator/AppNavigation.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Routes } from '../routes';
-import { Service, Services } from '../../screens';
-import { AppParamList } from './types';
+import { Result, Results, Service, Services } from '../../screens';
 import AppDrawerContent from './AppDrawerContent';
 
-const AppDrawer = createDrawerNavigator<AppParamList>();
-const ServicesStack = createStackNavigator<{ Services: undefined; Service: undefined }>();
+const AppDrawer = createDrawerNavigator();
+const ServicesStack = createStackNavigator();
+const ResultsStack = createStackNavigator();
+
+const ResultsStackStackNavigator = () => (
+  <ResultsStack.Navigator>
+    <ResultsStack.Screen name="Results" component={Results} options={{ headerShown: false }} />
+    <ResultsStack.Screen name="Result" component={Result} options={{ headerShown: false }} />
+  </ResultsStack.Navigator>
+);
 
 const ServicesStackNavigator = () => (
   <ServicesStack.Navigator>
@@ -18,10 +24,11 @@ const ServicesStackNavigator = () => (
 
 const AppDrawerNavigator = () => (
   <AppDrawer.Navigator
-    initialRouteName={Routes.Services}
+    initialRouteName="Services"
     drawerContent={(props) => <AppDrawerContent {...props} />}
   >
-    <AppDrawer.Screen name={Routes.Services} component={ServicesStackNavigator} />
+    <AppDrawer.Screen name="Services" component={ServicesStackNavigator} />
+    <AppDrawer.Screen name="Results" component={ResultsStackStackNavigator} />
   </AppDrawer.Navigator>
 );
 
