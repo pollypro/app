508fa33833b7394d3e1fa2c77f47c82a8f504c5a
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-28 15:34:25 +0300

    add missing exports of AppLayout and Header

--- src/screens/shared/index.ts
+++ src/screens/shared/index.ts
@@ -1,1 +1,3 @@
+export { default as AppLayout } from './AppLayout/AppLayout';
+export { default as Header } from './Header/Header';
 export { default as ScreenLayout } from './ScreenLayout/ScreenLayout';
