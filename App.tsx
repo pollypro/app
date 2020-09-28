b0139d0c9c5751b4e9fe75c8fdfa55046a28ad77
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-29 02:35:00 +0300

    extract Main, connect to props, get and check the token

--- App.tsx
+++ App.tsx
@@ -1,25 +1,67 @@
 // imports from vendors
-import React from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import AsyncStorage from '@react-native-community/async-storage';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
-
 import { Provider as ReduxProvider } from 'react-redux';
+import { ConnectedProps, connect } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // imports from navigation
 import RootNavigator from './src/navigation/RootNavigator/RootNavigator';
 
+// imports from modules
+import { getSelf } from './src/modules/self';
+import { replaceAuth, destroyAuth } from './src/modules/auth';
+
 import reduxStore from './src/reduxStore';
+
+const reduxConnector = connect(null, { getSelf, replaceAuth, destroyAuth });
+
+type Props = ConnectedProps<typeof reduxConnector>;
+
+const Main: FC<Props> = ({ getSelf, replaceAuth, destroyAuth }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (isLoading) {
+      const job = async () => {
+        const token = await AsyncStorage.getItem('token');
+
+        if (token) {
+          replaceAuth(token)
+            .then(() => getSelf())
+            .catch(() => replaceAuth(null))
+            .then(() => setIsLoading(false));
+        } else {
+          destroyAuth().then(() => setIsLoading(false));
+        }
+      };
+
+      job();
+    }
+  }, [isLoading]);
+
+  if (!isLoading) {
+    return (
+      <SafeAreaProvider>
+        <RootNavigator />
+      </SafeAreaProvider>
+    );
+  }
+
+  return null;
+};
+
+const MainConnected = reduxConnector(Main);
 
 const App = () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
       <ReduxProvider store={reduxStore}>
-        <SafeAreaProvider>
-          <RootNavigator />
-        </SafeAreaProvider>
+        <MainConnected />
       </ReduxProvider>
     </ApplicationProvider>
   </>
