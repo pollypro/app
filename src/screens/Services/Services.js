d70b90408ac5b1f65c882c713e9d76c906f2bc1a
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-10-01 03:40:15 +0300

    Services: map getServices, cleanupServices, call getServices on mount, cleanupServices on unmount

--- src/screens/Services/Services.js
+++ src/screens/Services/Services.js
@@ -1,15 +1,26 @@
 // imports from vendors
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import { Text } from '@ui-kitten/components';
+import { connect } from 'react-redux';
 
 // imports from components
 import AppLayout from '../shared/AppLayout/AppLayout';
 
+// imports from modules
+import { getServices, cleanupServices } from '../../modules/services';
+
 // imports from styles
 import styles from './styles';
 
-const Services = ({ navigation }) => {
+const Services = ({ navigation, getServices, cleanupServices }) => {
+  useEffect(() => {
+    getServices();
+    return () => {
+      cleanupServices();
+    };
+  }, []);
+
   return (
     <AppLayout title="Услуги">
       <View style={styles.content}>
@@ -19,4 +30,4 @@ const Services = ({ navigation }) => {
   );
 };
 
-export default Services;
+export default connect(null, { getServices, cleanupServices })(Services);
